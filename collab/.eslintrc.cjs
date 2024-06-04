5975c6e6741d0ae43391649afd33d9e4c7782189
David Furey <david.furey@guardian.co.uk>
2024-06-04 18:05:56 +0100

    Lint rule: strict-boolean-expressions

--- collab/.eslintrc.cjs
+++ collab/.eslintrc.cjs
@@ -4,6 +4,20 @@ module.exports = {
       {
         files: ["*.ts", "*.tsx"],
         extends: ["@guardian/eslint-config-typescript"],
+        rules: {
+            "@typescript-eslint/strict-boolean-expressions": [
+                "error",
+                {
+                    allowString: false,
+                    allowNumber: false,
+                    allowNullableObject: false,
+                    allowNullableBoolean: false,
+                    allowNullableString: false,
+                    allowNullableNumber: false,
+                    allowAny: false,
+                }
+            ],
+        },
       },
     ],
   };
