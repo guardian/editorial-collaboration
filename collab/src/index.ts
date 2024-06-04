3ee1788dcc5ec414fc89292034e109c7fdb52513
David Furey <david.furey@guardian.co.uk>
2024-06-04 16:23:48 +0100

    Create route to receive steps
    Just logs steps and throws them away. This will unblock client side development. We can add persistence later

--- collab/src/index.ts
+++ collab/src/index.ts
@@ -4,12 +4,25 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json()) 
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
 app.get('/healthcheck', (req: Request, res: Response) => {
     res.send('OK');
+});
+
+app.post('/documents/:id/steps', (req: Request, res: Response) => {
+  console.log(
+    {
+      requestBody: req.body,
+      documentId: req.params.id,
+    }
+  );
+  res.status(202);
+  res.send("Received");
 });
 
 app.listen(port, () => {
