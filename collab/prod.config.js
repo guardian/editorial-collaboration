f71a20110366f829680853e4b065c7bd2e4f57b3
David Furey <david.furey@guardian.co.uk>
2024-06-05 13:15:23 +0100

    Compile TS to JS as part of webpack
    This avoids `tsc` leaving `.js` files hanging around in `src/`

--- collab/prod.config.js
+++ collab/prod.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+    entry: './src/index.ts',
     mode: 'production',
-    target: 'node'
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
 };
