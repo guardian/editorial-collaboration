1a0e82d57fad6a376e75e4aff21af8b39996ff95
Phill Barron <phillipbarron242@gmail.com>
2024-06-05 10:27:54 +0100

    Added testing framework - moved server start out to server.ts to simplify testing Added tests around existing routes

--- collab/src/index.ts
+++ collab/src/index.ts
@@ -2,7 +2,6 @@ import type { Request, Response } from "express";
 import express from 'express';
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
@@ -12,6 +11,6 @@ app.get('/healthcheck', (req: Request, res: Response) => {
     res.send('OK');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+
+export default app;
