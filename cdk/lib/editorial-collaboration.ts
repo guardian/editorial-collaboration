415be259da9b60420903e7eb6fc13b9d6bf8a186
fweddi <freddiepreece@outlook.com>
2024-06-13 11:21:15 +0100

    Add initial Postgres DB

--- cdk/lib/editorial-collaboration.ts
+++ cdk/lib/editorial-collaboration.ts
@@ -3,9 +3,17 @@ import { AccessScope } from '@guardian/cdk/lib/constants';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuCname } from '@guardian/cdk/lib/constructs/dns';
+import { GuSecurityGroup, GuVpc } from '@guardian/cdk/lib/constructs/ec2';
+import { GuDatabaseInstance } from '@guardian/cdk/lib/constructs/rds';
 import type { App } from 'aws-cdk-lib';
-import { Duration } from 'aws-cdk-lib';
-import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
+import { Duration, aws_rds as rds, RemovalPolicy } from 'aws-cdk-lib';
+import {
+	InstanceClass,
+	InstanceSize,
+	InstanceType,
+	Port,
+} from 'aws-cdk-lib/aws-ec2';
+import { SubnetGroup } from 'aws-cdk-lib/aws-rds';
 
 interface EditorialCollaborationProps extends GuStackProps {
 	domainName: string;
@@ -18,11 +26,15 @@ export class EditorialCollaboration extends GuStack {
 		const { domainName, stage } = props;
 
 		const appName = 'editorial-collaboration';
+		const appPort = 3000;
 
-		const guPlayApp = new GuEc2App(this, {
+		const dbUsername = 'editorial_collaboration';
+		const dbPort = 5432;
+
+		const guEc2App = new GuEc2App(this, {
 			access: { scope: AccessScope.PUBLIC },
 			app: appName,
-			applicationPort: 3000,
+			applicationPort: appPort,
 			certificateProps: {
 				domainName,
 			},
@@ -67,7 +79,52 @@ systemctl start ${appName}
 			app: appName,
 			domainName: domainName,
 			ttl: Duration.hours(1),
-			resourceRecord: guPlayApp.loadBalancer.loadBalancerDnsName,
+			resourceRecord: guEc2App.loadBalancer.loadBalancerDnsName,
+		});
+
+		const dbAccessSecurityGroup = new GuSecurityGroup(this, 'DBSecurityGroup', {
+			app: appName,
+			description: 'Allow traffic from EC2 instance to DB',
+			vpc: guEc2App.vpc,
+			allowAllOutbound: false,
+		});
+
+		dbAccessSecurityGroup.connections.allowFrom(
+			guEc2App.autoScalingGroup,
+			Port.tcp(dbPort),
+			'Allow connections from EC2 instance to DB',
+		);
+
+		new GuDatabaseInstance(this, 'Database', {
+			app: `${appName}-db`,
+			instanceIdentifier: `${appName}-db-${stage}`,
+			instanceType: 'db.t4g.micro',
+			engine: rds.DatabaseInstanceEngine.postgres({
+				version: rds.PostgresEngineVersion.VER_16_2,
+			}),
+			vpc: guEc2App.vpc,
+			subnetGroup: new SubnetGroup(this, 'DBSubnetGroup', {
+				vpc: guEc2App.vpc,
+				vpcSubnets: {
+					subnets: GuVpc.subnetsFromParameter(this),
+				},
+				description: 'Subnet for editorial collaboration database',
+			}),
+			port: dbPort,
+			preferredMaintenanceWindow: 'Mon:06:30-Mon:07:00',
+			credentials: rds.Credentials.fromGeneratedSecret(dbUsername),
+			iamAuthentication: true,
+			storageType: rds.StorageType.GP2, // SSD
+			allocatedStorage: 150,
+			storageEncrypted: true,
+			multiAz: stage === 'PROD',
+			publiclyAccessible: false,
+			removalPolicy: RemovalPolicy.RETAIN,
+			securityGroups: [dbAccessSecurityGroup],
+			devXBackups: { enabled: true },
+			allowMajorVersionUpgrade: false,
+			autoMinorVersionUpgrade: true,
+			deleteAutomatedBackups: false,
 		});
 	}
 }
