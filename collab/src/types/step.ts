06584d52b7cb90f7d559ddea6af3602410457fad
David Blatcher <dv.bltchr@gmail.com>
2024-06-18 19:09:06 +0100

    add comments explaining the jsonID / Step / stepType relationship

--- collab/src/types/step.ts
+++ collab/src/types/step.ts
@@ -21,12 +21,31 @@ const defaultStepClasses = [
   DocAttrStep,
 ];
 
+/**
+ * Under the hood "prosemirror-transform" assigns a `jsonID` property to each of 
+ * the default subclasses of `Step`. It corresponds to value of the `stepType`
+ * property of the object returned by the subclass's `toJSON` method.
+ * 
+ * E.G. `ReplaceStep.jsonID` is "replace" and `replaceStep.toJson` returns an object
+ * with a `stepType` of "replace".
+ * 
+ * `jsonID` is used internally by `Step.fromJson` to look up the right subclass
+ * to deserialise JSON to.
+ * 
+ * We can use it here to derive the list of possible `stepTypes` that a valid
+ * JSON serialised `Step` can have
+ */
 const defaultStepTypes = defaultStepClasses.map(
   (stepClass) =>
     (stepClass.prototype as unknown as Step & { jsonID: string }).jsonID
 );
 
-// If we define custom steps, we would need to add their step types here.
+/**
+ * Prosemirror allows developer to define custom subclasses of `Step` (see
+ * the annotation on the `Step` type).
+ * 
+ * If we do this, we would need to add the `stepType` here.
+ */
 const ourCustomStepTypes: string[] = [];
 
 export const stepJsonIds = [
