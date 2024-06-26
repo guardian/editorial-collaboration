ae1ff212e8513623e3cba6b8283815cd1414eb57
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2024-06-26 09:46:59 +0100

    add cors headers

--- collab/src/index.ts
+++ collab/src/index.ts
@@ -1,14 +1,29 @@
 /* eslint-disable @typescript-eslint/no-misused-promises -- fixes middleware linting warning, which is a known issue with Express typings, see: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/50871*/
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from 'express';
 /* eslint "import/no-named-as-default-member": "off" -- This is the only way I can see to avoid a warning about the (correct) way express is imported below */
 import express from 'express';
-import {parseSteps} from "./lib/parse-steps";
-import {authMiddleware} from "./middleware/auth-middleware";
-import type { Json } from "./types/json";
+import { parseSteps } from './lib/parse-steps';
+import { authMiddleware } from './middleware/auth-middleware';
+import type { Json } from './types/json';
 
 export const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const host = req.get('origin') ?? '';
+  if (
+    host.endsWith('.gutools.co.uk') ||
+    host.endsWith('.dev-gutools.co.uk')
+  ) {
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', host);
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,POST,GET');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Max-Age', '86400');
+  }
+  next();
+});
 
 app.get('/', authMiddleware, (req: Request, res: Response) => {
     res.send('Hello World!');
