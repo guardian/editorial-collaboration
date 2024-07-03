6a4d28fd4d89ec1d4857da5edb436618417623a2
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2024-07-03 09:45:51 +0100

    add cors policy

--- collab/src/index.ts
+++ collab/src/index.ts
@@ -1,15 +1,22 @@
 /* eslint-disable @typescript-eslint/no-misused-promises -- fixes middleware linting warning, which is a known issue with Express typings, see: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/50871*/
+import cors from 'cors';
 import type { Request, Response } from 'express';
 /* eslint "import/no-named-as-default-member": "off" -- This is the only way I can see to avoid a warning about the (correct) way express is imported below */
 import express from 'express';
+import { DOMAIN } from './constants';
 import { database } from './lib/database';
 import { parseSteps } from './lib/parse-steps';
 import { authMiddleware } from './middleware/auth-middleware';
-import type { Json } from "./types/json";
+import type { Json } from './types/json';
 
 export const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({
+  origin: `https://composer.${DOMAIN}`,
+  credentials: true
+}));
 
 app.get('/', authMiddleware, (req: Request, res: Response) => {
     res.send('Hello World!');
--- collab/src/panDomainAuth.ts
+++ collab/src/panDomainAuth.ts
@@ -3,17 +3,10 @@ import {
     guardianValidation,
     PanDomainAuthentication
 } from "@guardian/pan-domain-node";
-import { AWS_REGION, STAGE } from "./constants";
-import type { Stage } from "./types/stage";
-
-const pandaConfigFilenameLookup: { [stage in Stage]: string } = {
-    PROD: "gutools.co.uk.settings",
-    CODE: "code.dev-gutools.co.uk.settings",
-    LOCAL: "local.dev-gutools.co.uk.settings",
-} as const;
+import { AWS_REGION, DOMAIN } from "./constants";
 
 export const pandaSettingsBucketName = "pan-domain-auth-settings";
-export const pandaPublicConfigFilename = `${pandaConfigFilenameLookup[STAGE]}.public`;
+export const pandaPublicConfigFilename = `${DOMAIN}.settings.public`;
 
 export const pandaCookieName = "gutoolsAuth-assym";
 
