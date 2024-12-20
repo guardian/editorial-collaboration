42668fec81e9f351d986154f0faf032836e39a18
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2024-12-20 14:05:40 +0000

    use buttons to incrementally apply steps to editor state

--- collab/demo/src/Editor.tsx
+++ collab/demo/src/Editor.tsx
@@ -4,7 +4,7 @@ import { Node } from 'prosemirror-model';
 import { EditorState } from 'prosemirror-state';
 import { Step } from 'prosemirror-transform';
 import { EditorView } from 'prosemirror-view';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import type { Json } from "../../src/types/json";
 import { schema } from '../lib/create-schema';
@@ -12,8 +12,8 @@ import { MONTH } from '../lib/month';
 
 type Content = {
   slice?: {
-    content: any;
-  }
+    content: unknown;
+  };
 }
 
 const style = {
@@ -38,31 +38,47 @@ const style = {
 }
 
 const Editor: React.FunctionComponent = () => {
-  const data = useLoaderData() as { steps: Array<{ content: Content, timestamp: string }>; doc: Json };
+  const data = useLoaderData() as { steps: Array<{ content: Content; timestamp: string }>; doc: Json };
+  const clientID = 'test';
+  const version = 1;
+  const attributes = { contentEditable: 'false' };
+  const doc = Node.fromJSON(schema, data.doc);
+  const initialState = EditorState.create({ doc, schema, plugins: [collab({ clientID, version })]});
   const editor = useRef<HTMLDivElement>(null);
   const initialised = useRef<boolean>(false);
+  const [view, setView] = useState<EditorView>();
 
   useEffect(() => {
     if (!initialised.current) {
       initialised.current = true;
-      const clientID = 'test';
-      const version = 1;
-      const doc = Node.fromJSON(schema, data.doc);
-      const state = EditorState.create({ doc, schema, plugins: [collab({ clientID, version })]});
-      const view = new EditorView(editor.current, {state, attributes: { contentEditable: 'false' }});
+      const view = new EditorView(editor.current, { state: initialState, attributes });
+      setView(view);
+      data.steps.forEach(step => console.log(JSON.stringify(step)));
       const steps = data.steps.map(step => Step.fromJSON(schema, step.content));
-      const transaction = receiveTransaction(state, steps, []);
+      const transaction = receiveTransaction(initialState, steps, []);
       view.dispatch(transaction);
     }
   }, []);
 
+  const onClick = (index: number) => {
+    view?.setProps({state: initialState, attributes });
+    const stepsToApply = data.steps.slice(0, index + 1);
+    const steps = stepsToApply.map(step => Step.fromJSON(schema, step.content));
+    const transaction = receiveTransaction(initialState, steps, []);
+    view?.dispatch(transaction);
+  }
+
   return (
     <div css={style.container}>
       <div css={style.editor} ref={editor} />
-      <div css={style.history}>{data.steps.map(step => {
+      <div css={style.history}>{data.steps.map((step, index) => {
         const date = new Date(step.timestamp);
         const dateString = `${date.getDay()} ${MONTH[date.getMonth() - 1]}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-        return (<button css={style.button}>{`${dateString} - ${JSON.stringify(step.content.slice?.content)}`}</button>)
+        return (
+          <button key={index} css={style.button} onClick={() => onClick(index)}>
+            {`${dateString} - ${JSON.stringify(step.content.slice?.content)}`}
+          </button>
+        );
       })}</div>
     </div>
   );
