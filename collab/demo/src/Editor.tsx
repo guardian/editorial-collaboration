d99ce888e0dfaa0f3777edfd64bbad9613024846
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2025-01-03 18:20:24 +0000

    start grouping steps into changesets

--- collab/demo/src/Editor.tsx
+++ collab/demo/src/Editor.tsx
@@ -1,7 +1,9 @@
 import { css } from '@emotion/react';
+import { ChangeSet, simplifyChanges } from 'prosemirror-changeset';
 import { collab, receiveTransaction } from 'prosemirror-collab';
 import { Node } from 'prosemirror-model';
 import { EditorState } from 'prosemirror-state';
+import type { StepMap } from 'prosemirror-transform';
 import { Step } from 'prosemirror-transform';
 import { Decoration, DecorationSet, EditorView } from 'prosemirror-view';
 import React, { useEffect, useRef, useState } from 'react';
@@ -16,6 +18,13 @@ type Content = {
   };
   to: number;
   from: number;
+}
+
+type Steps = Array<{ content: Content; timestamp: string }>;
+
+type Selection = {
+  from: number;
+  to: number;
 }
 
 const style = {
@@ -41,15 +50,20 @@ const style = {
   })
 }
 
-const isDeletion = (from: number, to: number): boolean => to > from
+const groupSteps = (steps: Steps): Selection[] => {
+  const length = steps.length;
+  return [
+    { from: 0, to: Math.floor(length / 2) },
+    { from: Math.floor(length / 2), to: length + 1 }
+  ];
+}
 
 const Editor: React.FunctionComponent = () => {
-  const data = useLoaderData() as { steps: Array<{ content: Content; timestamp: string }>; doc: Json };
+  const data = useLoaderData() as { steps: Steps; doc: Json };
   const clientID = 'test';
   const version = 1;
   const attributes = { contentEditable: 'false' };
   const doc = Node.fromJSON(schema, data.doc);
-  const initialState = EditorState.create({ doc, schema, plugins: [collab({ clientID, version })]});
   const editor = useRef<HTMLDivElement>(null);
   const initialised = useRef<boolean>(false);
   const [view, setView] = useState<EditorView>();
@@ -57,43 +71,56 @@ const Editor: React.FunctionComponent = () => {
   useEffect(() => {
     if (!initialised.current) {
       initialised.current = true;
-      const view = new EditorView(editor.current, { state: initialState, attributes });
+      const state = EditorState.create({ doc, schema, plugins: [collab({ clientID, version })]});
+      const view = new EditorView(editor.current, { state, attributes });
       setView(view);
-      data.steps.forEach(step => console.log(JSON.stringify(step)));
       const steps = data.steps.map(step => Step.fromJSON(schema, step.content));
-      const transaction = receiveTransaction(initialState, steps, []);
+      const transaction = receiveTransaction(state, steps, []);
       view.dispatch(transaction);
     }
   }, []);
 
-  const onClick = (index: number, content: Content) => {
-    const { from, to } = content;
-    const deleting = isDeletion(from, to);
-    const decorations: Decoration[] = deleting ?
-      [Decoration.inline(from, to + 2, { style: 'color:#00826a;white-space:pre;text-decoration:line-through' })] :
-      [Decoration.inline(from, to + 2, { style: 'color:#00826a;background-color:#dcf5f0;white-space:pre' })];
+  const onClick = (selection: Selection) => {
+    // Reset the editor state
+    const state: EditorState = EditorState.create({ doc, schema, plugins: [collab({ clientID, version })]});
+
+    // Generate a simplified changeset grouping steps into insertions and deletions.
+    const steps: Step[] = data.steps.slice(selection.from, selection.to).map(step => Step.fromJSON(schema, step.content));
+    const stepMaps: StepMap[] = steps.map(step => step.getMap());
+    const changeSet = ChangeSet.create(state.doc).addSteps(state.doc, stepMaps, data);
+    const simplifiedChangeSet = simplifyChanges(changeSet.changes, changeSet.startDoc);
+
+    // Apply text decorations to insertions and deletions.
+    const decorations: Decoration[] = simplifiedChangeSet.map(change => [
+      Decoration.inline(change.fromB, change.toB, { style: 'color:#00826a;background-color:#dcf5f0;white-space:pre' }),
+      Decoration.inline(change.fromA, change.toA, { style: 'color:#00826a;white-space:pre;text-decoration:line-through' })
+    ]).flat();
+
+    // Apply the changes to the editor view.
     view?.setProps({
-      state: initialState,
+      state,
       attributes,
       decorations: (state: EditorState) => DecorationSet.create(state.doc, decorations)
     });
-    const stepsToApply = data.steps.slice(0, deleting ? index + 1 + (from - to) : index + 1);
-    const steps = stepsToApply.map(step => Step.fromJSON(schema, step.content));
-    const transaction = receiveTransaction(initialState, steps, []);
+    const stepsToApply = data.steps.slice(0, selection.to).map(step => Step.fromJSON(schema, step.content));
+    const transaction = receiveTransaction(state, stepsToApply, []);
     view?.dispatch(transaction);
   }
 
   return (
     <div css={style.container}>
       <div css={style.editor} ref={editor} />
-      <div css={style.history}>{data.steps.map((step, index) => {
-        const date = new Date(step.timestamp);
-        const dateString = `${date.getDay()} ${MONTH[date.getMonth() - 1]}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-        return (
-          <button key={index} css={style.button} onClick={() => onClick(index, step.content)}>
-            {`${dateString} - ${JSON.stringify(step.content.slice?.content)}`}
-          </button>
-        );
+      <div css={style.history}>{groupSteps(data.steps).map((selection, index) => {
+        const firstStep = data.steps[selection.from];
+        if (firstStep != null) {
+          const date = new Date(firstStep.timestamp);
+          return (
+            <button key={index} css={style.button} onClick={() => onClick(selection)}>
+              {`${date.getDate()} ${MONTH[date.getMonth()]}, ${date.getHours()}:${date.getMinutes()}`}
+            </button>
+          );
+        }
+        return;
       })}</div>
     </div>
   );
